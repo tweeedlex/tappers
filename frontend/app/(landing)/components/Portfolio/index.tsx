0e91c2392f62e5eaa76163d60cbf90eeaace20ce
asd <7545245@ukr.net>
2025-01-03 20:41:58 +0200

    made portfolio slider responsive

--- frontend/app/(landing)/components/Portfolio/index.tsx
+++ frontend/app/(landing)/components/Portfolio/index.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useRef, useEffect } from 'react';
+import React, {useRef, useEffect, useState} from 'react';
 import { Swiper, SwiperSlide } from "swiper/react";
 import styles from "./Portfolio.module.scss";
 import classNames from "classnames";
@@ -8,12 +8,30 @@ import { Mousewheel, Controller } from 'swiper/modules';
 import 'swiper/css';
 
 const Portfolio = () => {
-  const items = [
+  const [isSmall, setIsSmall] = useState(false);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsSmall(window.innerWidth < 1023);
+    };
+
+    handleResize();
+    window.addEventListener('resize', handleResize);
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
+  useEffect(() => {
+    setSlidersAmount(isSmall ? 2 : 4);
+  }, [isSmall]);
+
+  const [slidersAmount, setSlidersAmount] = useState(4);
+
+  const items = new Array(slidersAmount / 2).fill(0).map(_ => [
     [{ img: "1.jpg" }, { img: "2.jpg" }, { img: "3.jpg" }, { img: "4.jpg" }, { img: "5.jpg" }, { img: "6.jpg" }],
     [{ img: "7.jpg" }, { img: "8.jpg" }, { img: "9.jpg" }, { img: "10.jpg" }, { img: "11.jpg" }, { img: "12.jpg" }],
-    [{ img: "13.jpg" }, { img: "14.jpg" }, { img: "15.jpg" }, { img: "16.jpg" }, { img: "17.jpg" }, { img: "18.jpg" }],
-    [{ img: "19.jpg" }, { img: "20.jpg" }, { img: "21.jpg" }, { img: "22.jpg" }, { img: "23.jpg" }, { img: "24.jpg" }]
-  ];
+  ]).flat();
 
   const swiperRefs = useRef([]);
   const activeIndexRef = useRef(0);
@@ -40,44 +58,47 @@ const Portfolio = () => {
     }, 350);  // Slightly longer than animation to ensure completion
   };
 
+
   return (
     <div>
       <h2 className="h2 text-center py-20 px-3">
         <span className="text-secondary">Unique</span> ideas generate <span className="text-secondary">impressive</span> apps
       </h2>
-      <div className="w-full h-[90vh] flex gap-4 overflow-hidden">
-        {items.map((itemGroup, i) => (
-          <Swiper
-            className={classNames(`swiper slider${i + 1} h-full`, styles.slider)}
-            key={i}
-            freeMode={false}
-            centeredSlides={true}
-            direction="vertical"
-            mousewheel={true}
-            speed={300}  // Added consistent animation speed
-            loop={true}
-            slidesPerView={1.75}
-            slidesOffsetBefore={-125}
-            modules={[Mousewheel, Controller]}
-            onSwiper={(swiper) => {
-              swiperRefs.current[i] = swiper;
-            }}
-            onSlideChange={(swiper) => handleSlideChange(swiper)}
-          >
-            <div className={classNames("swiper-wrapper", styles.sliderWrapper)}>
-              {itemGroup.map((item) => (
-                <SwiperSlide key={item.img}>
-                  <div className={classNames("swiper-slide", styles.sliderItem)}>
-                    <div
-                      className={classNames(styles.sliderImg)}
-                      style={{ backgroundImage: `url(img/landing/portfolio/${item.img})` }}
-                    />
-                  </div>
-                </SwiperSlide>
-              ))}
-            </div>
-          </Swiper>
-        ))}
+      <div className={"overflow-hidden"}>
+        <div className="w-full h-[80vh] scale-[115%] flex gap-3">
+          {items.map((itemGroup, i) => (
+            <Swiper
+              className={classNames(`swiper slider${i + 1} h-full`, styles.slider)}
+              key={i}
+              freeMode={false}
+              centeredSlides={true}
+              direction="vertical"
+              mousewheel={true}
+              speed={300}  // Added consistent animation speed
+              loop={true}
+              slidesPerView={1.75}
+              slidesOffsetBefore={-125}
+              modules={[Mousewheel, Controller]}
+              onSwiper={(swiper) => {
+                swiperRefs.current[i] = swiper;
+              }}
+              onSlideChange={(swiper) => handleSlideChange(swiper)}
+            >
+              <div className={classNames("swiper-wrapper", styles.sliderWrapper)}>
+                {itemGroup.map((item) => (
+                  <SwiperSlide key={item.img}>
+                    <div className={classNames("swiper-slide", styles.sliderItem)}>
+                      <div
+                        className={classNames(styles.sliderImg)}
+                        style={{ backgroundImage: `url(img/landing/portfolio/${item.img})` }}
+                      />
+                    </div>
+                  </SwiperSlide>
+                ))}
+              </div>
+            </Swiper>
+          ))}
+        </div>
       </div>
     </div>
   );
