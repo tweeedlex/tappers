c736dec1d1e628321c35ca51840c442d484c7b9b
krutiy <7545245@ukr.net>
2025-01-08 21:50:28 +0200

    fixed contact component

---
+++ frontend/app/(landing)/components/ContactUs/Form/index.tsx
@@ -0,0 +1,61 @@
+"use client";
+import React, {FC, useState} from 'react';
+import Input from "@/components/ui/Input";
+import TextArea from "@/components/ui/TextArea";
+import Button from "@/components/ui/Button";
+import {toastError, toastSuccess} from "@/helpers/toasts";
+import {addContact} from "@/requests/contact";
+
+const Form: FC = () => {
+  const [formInfo, setFormInfo] = useState({
+    name: "",
+    email: "",
+    budget: "",
+    message: ""
+  });
+
+  const formValidation = () => {
+    return !(!formInfo.name || !formInfo.email || !formInfo.budget || !formInfo.message);
+  }
+
+  const handleSubmit = async () => {
+    if (!formValidation()) {
+      return toastError("Please fill all the fields");
+    }
+    const response = await addContact(formInfo);
+
+    if (response.error) {
+      return toastError("Unexpected error occurred. Please try again later.");
+    }
+    toastSuccess("Thank you for your message! We will contact you soon.");
+  };
+
+  return (
+    <div className={"flex w-full max-w-[600px] flex-col gap-3 items-center"}>
+      <div className={"flex w-full flex-col gap-1 items-start"}>
+        <p className={"text-sm text-primary"}>Name</p>
+        <Input className={"w-full"} placeholder={"Nik"} value={formInfo.name}
+               onChange={(value: string) => setFormInfo({...formInfo, name: value})}/>
+      </div>
+      <div className={"flex w-full flex-col gap-1 items-start"}>
+        <p className={"text-sm text-primary"}>E-Mail</p>
+        <Input className={"w-full"} placeholder={"nik@mail.com"} value={formInfo.email}
+               onChange={(value: string) => setFormInfo({...formInfo, email: value})}/>
+      </div>
+      <div className={"flex w-full flex-col gap-1 items-start"}>
+        <p className={"text-sm text-primary"}>Your estimated budget</p>
+        <Input className={"w-full"} placeholder={"2000$"} value={formInfo.budget}
+               onChange={(value: string) => setFormInfo({...formInfo, budget: value})}/>
+      </div>
+      <div className={"flex w-full flex-col gap-1 items-start"}>
+        <p className={"text-sm text-primary"}>Message</p>
+        <TextArea className={"w-full h-[100px]"} placeholder={"Feel free to share your thoughts on the project!"}
+                  value={formInfo.message} onChange={(value: string) => setFormInfo({...formInfo, message: value})}/>
+      </div>
+      <Button onClick={handleSubmit} variant={"primary"}
+              className={"m:min-w-[unset] mt-4 m:max-w-[fit-content] min-w-full mac-w-unset'"}>Submit</Button>
+    </div>
+  );
+};
+
+export default Form;
--- frontend/app/(landing)/components/ContactUs/index.tsx
+++ frontend/app/(landing)/components/ContactUs/index.tsx
@@ -1,62 +1,17 @@
-"use client";
-
-import React, {FC, useState} from 'react';
-import Input from "@/components/ui/Input";
-import TextArea from "@/components/ui/TextArea";
-import Button from "@/components/ui/Button";
+import React, {FC} from 'react';
 import Container from "@/components/layout/Container";
-import {addContact} from "@/requests/contact";
-import {toastError, toastSuccess} from "@/helpers/toasts";
 import SocialLinks from "@/components/misc/SocialLinks";
+import Form from "@/app/(landing)/components/ContactUs/Form";
 
 const ContactUs: FC = () => {
-  const [formInfo, setFormInfo] = useState({
-    name: "",
-    email: "",
-    budget: "",
-    message: ""
-  });
 
-  const formValidation = () => {
-    return !(!formInfo.name || !formInfo.email || !formInfo.budget || !formInfo.message);
-  }
-
-  const handleSubmit = async () => {
-    if (!formValidation()) {
-      return toastError("Please fill all the fields");
-    }
-    const response = await addContact(formInfo);
-
-    if (response.error) {
-      return toastError("Unexpected error occurred. Please try again later.");
-    }
-    toastSuccess("Thank you for your message! We will contact you soon.");
-  };
 
   return (
     <Container className={"contact flex flex-col gap-5 items-center"}>
         <p className={"text-secondary text-sm"}>Contact us</p>
         <h2 className={"h2"}>Let&#39;s Talk!</h2>
         <p className={"subtitle"}>We know that you want to implement your idea in reality. Why shouldn&#39;t we do this together?</p>
-        <div className={"flex w-full max-w-[600px] flex-col gap-3 items-center"}>
-          <div className={"flex w-full flex-col gap-1 items-start"}>
-            <p className={"text-sm text-primary"}>Name</p>
-            <Input className={"w-full"} placeholder={"Nik"} value={formInfo.name} onChange={(value: string) => setFormInfo({...formInfo, name: value})}/>
-          </div>
-          <div className={"flex w-full flex-col gap-1 items-start"}>
-            <p className={"text-sm text-primary"}>E-Mail</p>
-            <Input className={"w-full"} placeholder={"nik@mail.com"} value={formInfo.email} onChange={(value: string) => setFormInfo({...formInfo, email: value})}/>
-          </div>
-          <div className={"flex w-full flex-col gap-1 items-start"}>
-            <p className={"text-sm text-primary"}>Your estimated budget</p>
-            <Input className={"w-full"} placeholder={"2000$"} value={formInfo.budget} onChange={(value: string) => setFormInfo({...formInfo, budget: value})}/>
-          </div>
-          <div className={"flex w-full flex-col gap-1 items-start"}>
-            <p className={"text-sm text-primary"}>Message</p>
-            <TextArea className={"w-full h-[100px]"} placeholder={"Feel free to share your thoughts on the project!"} value={formInfo.message} onChange={(value: string) => setFormInfo({...formInfo, message: value})}/>
-          </div>
-          <Button onClick={handleSubmit} variant={"primary"} className={"m:min-w-[unset] mt-4 m:max-w-[fit-content] min-w-full mac-w-unset'"}>Submit</Button>
-        </div>
+        <Form />
       <SocialLinks />
     </Container>
   );
